08fbca30256d0032daf89e264f0fd5b9e096649e
Matthew Spire <matthew_spire@yahoo.com>
2020-06-02 08:54:09 -0700

    made changes suggested by Filip

--- Week 1/blackjack/src/blackjack.kt
+++ Week 1/blackjack/src/blackjack.kt
@@ -1,43 +1,39 @@
 // Create a data class called Card with two members, a String for the pip and a Char for the suit
 data class Card(
-    val pip: String,
-    val suit: Char
+        val pip: String,
+        val suit: Char
 )
 
 // Implementation of the createDeck function. Returns the MutableSet<Card>
 fun createDeck(): MutableSet<Card> {
     // Create a collection of suits using unicode characters for the suit
-    val suit = setOf('\u2663', '\u2660', '\u2666', '\u2665')
+    val suits = setOf('\u2663', '\u2660', '\u2666', '\u2665')
     // Create a collection of pips
-    val pip = setOf("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+    val pips = setOf("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
 
     // Create a MutableSet<Card>
     val cards = mutableSetOf<Card>()
 
     // Use a nested for loop to create a card of each pip for each suit and place it in the MutableSet<Card>
-    pip.forEach { PIP -> suit.forEach { SUIT -> cards.add(Card(PIP, SUIT)) } }
+    pips.forEach { pip -> suits.forEach { suit -> cards.add(Card(pip, suit)) } }
 
     // Return the MutableSet<Card>, which is a standard 52-card deck
     return cards
 }
 
 /*
- * Implementation of the dealHand function. Receives the MutableSet<Card> as a parameter and returns a collection of
- * two cards
+ * Implementation of the dealHand function. Receives the MutableSet<Card> and sizeOfHand as parameters and returns a
+ * collection of two cards
  */
-fun dealHand(deck: MutableSet<Card>): MutableSet<Card> {
+fun dealHand(deck: MutableSet<Card>, sizeOfHand: Int): MutableSet<Card> {
     // Create a collection to store the cards dealt in the hand
     val handDealt = mutableSetOf<Card>()
 
-    // Number of cards in the hand
-    var sizeOfHand = 2
-
-    // while loop that deals two cards, adds each card dealt to the hand, and removes the cards dealt from the deck
-    while (sizeOfHand > 0) {
+    // Deal the hand using a for loop, ensuring that the card is removed from the deck after it is dealt
+    for (i in 1..sizeOfHand) {
         val card = deck.random()
         handDealt.add(card)
         deck.remove(card)
-        sizeOfHand--
     }
 
     // Return the handDealt, which is a collection containing the number of cards specified (i.e. two cards)
@@ -52,23 +48,16 @@ fun evaluateHand(handDealt: MutableSet<Card>): Int {
     // Declare a variable to store the total of the pip values
     var cardTotal = 0
 
-    // Extract the first card in the hand
-    val firstCard = handDealt.elementAt(0)
-    // Extract the second card in the hand
-    val secondCard = handDealt.elementAt(1)
-
-    // when expression that figures out the pip value of the first card in the hand and adds its value to the total
-    when (firstCard.pip) {
-        "2", "3", "4", "5", "6", "7", "8", "9", "10" -> cardTotal += firstCard.pip.toInt()
-        "J", "Q", "K" -> cardTotal += 10
-        "A" -> cardTotal += 11
-    }
-
-    // when expression that figures out the pip value of the second card in the hand and adds its value to the total
-    when (secondCard.pip) {
-        "2", "3", "4", "5", "6", "7", "8", "9", "10" -> cardTotal += secondCard.pip.toInt()
-        "J", "Q", "K" -> cardTotal += 10
-        "A" -> cardTotal += 11
+    // for loop implementation to evaluate the hand
+    for (card in handDealt) {
+        val pip = card.pip
+        cardTotal += if (pip == "A") {
+            11
+        } else if (pip == "J" || pip == "Q" || pip == "K") {
+            10
+        } else {
+            pip.toInt()
+        }
     }
 
     // Return the cardTotal, which is an integer that is the sum of the pip values of the two cards
@@ -80,16 +69,15 @@ fun evaluateHand(handDealt: MutableSet<Card>): Int {
  * the handEvaluated (i.e. the total of the pip values of the two cards).
  */
 fun printResults(handDealt: MutableSet<Card>, handEvaluated: Int) {
-    // Extract the first card in the hand
-    val firstCard = handDealt.elementAt(0)
-    // Extract the second card in the hand
-    val secondCard = handDealt.elementAt(1)
-
     println("Your hand was:")
-    // Print the pip and suit of the first card
-    println(firstCard.pip + firstCard.suit)
-    // Print the pip and suit of the second card
-    println(secondCard.pip + secondCard.suit)
+
+    // Print the pip and suit of each card in the hand
+    for (card in handDealt) {
+        val pip = card.pip
+        val suit = card.suit
+        println(pip + suit)
+    }
+
     // Print the total of the hand
     println("For a total of: $handEvaluated")
 
@@ -99,7 +87,7 @@ fun printResults(handDealt: MutableSet<Card>, handEvaluated: Int) {
      */
     if (handEvaluated == 21) {
         println("You Win!")
-    } else if (handEvaluated == 22) {
+    } else if (handEvaluated > 21) {
         println("You Lose!")
     }
 }
@@ -109,8 +97,11 @@ fun main() {
     // Call the createDeck function to create the deck of playing cards
     val deck = createDeck()
 
+    // Size of the hand to be dealt
+    val sizeOfHand = 2
+
     // Call the dealHand function to deal the hand using the created deck of playing cards
-    val handDealt = dealHand(deck)
+    val handDealt = dealHand(deck, sizeOfHand)
 
     // Call the evaluateHand function to determine the pip total of the two cards in the hand dealt
     val handEvaluated = evaluateHand(handDealt)
